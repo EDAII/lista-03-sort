8eab886367f1447add1b50262f5eeca5e70f15fa
jppgomes <joaok8@gmail.com>
2018-09-08 12:19:11 -0300

    ädd insertion sort

---
+++ insertion.py
@@ -0,0 +1,15 @@
+def insertion_sort(vector):
+	i = 1
+	# i go to end of the vector
+	while i < len(vector):
+		temporaria = vector[i]
+		swap = False
+		j = i - 1 #element to compair
+		while j >= 0 and vector[j] > temporaria:
+			vector[j+1] = vector[j]
+			swap = True #swap
+			j = j - 1
+		if swap:
+			vector[j+1] = temporaria
+		i = i + 1
+
